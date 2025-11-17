5ed815157a67c62fbba4ccfbaa709e117d2e01ca
ArianSdg <ariansadegh8305@gmail.com>
2025-11-17 21:46:26 +0330

    Debug log_activity function

--- library_manager/app/core/logger.py
+++ library_manager/app/core/logger.py
@@ -7,16 +7,18 @@ def log_activity(action, book, message=""):
     now = datetime.now()
     timestamp = now.strftime("%Y-%m-%h %H-%M-%S")
 
+    book_info = f"'{book.title}' by {book.author} {book.year}" if book else "N/A"
+
     if action == "ADD":
-        log_message = f"[{timestamp}] ADD BOOK: '{book.title}' by {book.author} {book.year} - {message}"
+        log_message = f"[{timestamp}] ADD BOOK: {book_info} - {message}"
     elif action == "REMOVE":
-        log_message = f"[{timestamp}] REMOVE BOOK: '{book.title}' by {book.author} {book.year} - {message}"
+        log_message = f"[{timestamp}] REMOVE BOOK: {book_info} - {message}"
     elif action == "BORROW":
-        log_message = f"[{timestamp}] BORROW BOOK: '{book.title}' by {book.author} {book.year} - {message}"
+        log_message = f"[{timestamp}] BORROW BOOK: {book_info} - {message}"
     elif action == "RETURN":
-        log_message = f"[{timestamp}] RETURN BOOK: '{book.title}' by {book.author} {book.year} - {message}"
+        log_message = f"[{timestamp}] RETURN BOOK: {book_info} - {message}"
     else:
-        log_message = f"[{timestamp}] {action} - {message}"
+        log_message = f"[{timestamp}] {action}: {message}"
 
     with open(LOG_FILE, "a") as f:
         f.write(log_message + "\n")
