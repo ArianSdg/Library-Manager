09265423bc25f0ca046f24d426803d4e92ad87a8
ArianSdg <ariansadegh8305@gmail.com>
2025-11-17 17:11:38 +0330

    main file complete to run the program

---
+++ library_manager/main.py
@@ -0,0 +1,9 @@
+from ui.cli import CLI
+from core.manager import LibraryManager
+from core.storage import Storage
+
+storage = Storage('data.books.json')
+manager = LibraryManager(storage)
+cli = CLI(manager)
+
+cli.menu()
