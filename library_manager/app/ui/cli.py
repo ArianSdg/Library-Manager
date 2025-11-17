60dcecf2426af5dd84f01085eaf47a6da8fab1ad
ArianSdg <ariansadegh8305@gmail.com>
2025-11-17 19:28:55 +0330

    Cleaning codes and adding sort on list books option

--- library_manager/app/ui/cli.py
+++ library_manager/app/ui/cli.py
@@ -1,3 +1,12 @@
+GREEN = "\033[92m"
+RED = "\033[91m"
+RESET = "\033[0m"
+
+def format_book_line(i, book):
+    status_color = GREEN if not book.is_borrowed else RED
+    status_text = "Available" if not book.is_borrowed else "Borrowed"
+    return f"{i}.{book.title} | {book.author} | {book.year} | {status_color}{status_text}{RESET}"
+
 class CLI:
     def __init__(self, manager):
         self.manager = manager
@@ -18,17 +27,27 @@ class CLI:
             if option == "1":
                 title = input("Title: ")
                 author = input("Author: ")
-                year = input("Year: ")
-                self.manager.add_book(title, author, year)
+                try:
+                    year = int(input("Year: "))
+                except ValueError:
+                    print("Invalid year! Must be a number.")
+                    continue
+
+                result = self.manager.add_book(title, author, year)
+                print(result)
+
             elif option == "2":
                 title = input("Title: ")
                 self.manager.delete_book(title)
+
             elif option == "3":
                 title = input("Title: ")
                 print(self.manager.borrow_book(title))
+
             elif option == "4":
                 title = input("Title: ")
                 print(self.manager.return_book(title))
+
             elif option == "5":
                 search_input = input("Search the title or the author: ")
                 searched_books = self.manager.search_books(search_input)
@@ -37,9 +56,17 @@ class CLI:
                 for s in searched_books:
                     print(f"{i}. {s.__str__()}")
                     i += 1
+
             elif option == "6":
-                books_list = self.manager.list_books()
-                print(books_list)
+                print("How do you want to sort the list?")
+                status = input("1.Title\n2.Author\n3.Year\n4.Borrowed status\n")
+
+                sorted_result = self.manager.list_books(status)
+                final_result = ""
+
+                for i, book in enumerate(sorted_result, 1):
+                    final_result += format_book_line(i, book) + "\n"
+
             elif option == "0":
                 break
             else:
